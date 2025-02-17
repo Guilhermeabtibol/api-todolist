83940562676e33ae3f08a32d2e753398a175c5af
guilhermeabtibol <guiabtibol@gmail.com>
2025-02-17 09:43:47 -0400

    Arquivos Inicias

---
+++ src/main/java/br/com/guilhermeabtibol/desafio_todolist/entity/Todo.java
@@ -0,0 +1,60 @@
+package br.com.guilhermeabtibol.desafio_todolist.entity;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String nome;
+    private String descricao;
+    private boolean realizado;
+    private int prioridades;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isRealizado() {
+        return realizado;
+    }
+
+    public void setRealizado(boolean realizado) {
+        this.realizado = realizado;
+    }
+
+    public int getPrioridades() {
+        return prioridades;
+    }
+
+    public void setPrioridades(int prioridades) {
+        this.prioridades = prioridades;
+    }
+}
